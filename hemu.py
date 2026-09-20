8f46fad318b276d99c7c8a0f21696d35214e2011
killihemanth2007-blip <killihemanth2007@gmail.com>
2026-09-20 09:35:24 +0530

    Add files via upload

---
+++ hemu.py
@@ -0,0 +1,84 @@
+print("Hello World")
+print("Welcome to Python")
+print("Killi Hemanth")
+print("I am learning python")
+print("python programming")
+print("Good Morning")
+print("Welcome Student")
+print("Python is easy")
+print("My First Program")
+print("Hello from Python")
+
+
+print("2 + 5")
+print(2+5)
+print("10 - 3")
+print(10-3)
+print("Hello World!")
+print("Python")
+print("Hello")
+print("10+20")
+print(10+20)
+
+
+name="Killi Hemanth"
+age=20
+marks=85
+price=99.5
+city="Chennai"
+score=95
+x=10
+message="Welcome"
+a=10
+b=20
+quantity=5
+price=10
+print(city)
+print(score)
+print(x)
+print(message)
+print(a+b)
+print(quantity*price)
+
+
+a=25
+b=25.5
+c="25"
+print(type(a))
+print(type(b))
+print(type(c))
+age=21
+print(type(age))
+height=5.8
+print(type(height))
+name="Arjun"
+print(type(name))
+age=20
+price=99.5
+name="Ravi"
+marks=90
+print(type(marks))
+temperature=36.5
+print(type(temperature))
+message="Welcome"
+print(type(message))
+
+
+a="start"
+b="Middle"
+c="End"
+print(a+" "+b+" "+c)
+x=10
+print(x)
+x=20
+print(x)
+Name="Ravi"
+age=20
+print(Name +" "+age)
+x=5
+y=10
+print(x+" "+y)
+a="login"
+s="Dashboard"
+n="Logout"
+print(a+" "+b+" "+c)
